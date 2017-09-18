95392ee04f3b6c58336b686e207c34c9951bca60
Tim <tja2353@gmail.com>
2017-09-18 11:08:04 +0100

    add IonicErrorHandler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { StatusBar } from '@ionic-native/status-bar';
-import { IonicApp, IonicModule } from 'ionic-angular';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 
@@ -116,6 +116,7 @@ import './rxjs-operators';
     TodoDataService,
     TodoService,
     ValidationService,
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
   ]
 })
 export class AppModule { }
